0d104798dbe60aee31e7cc6c260aebb9ee08350d
chase coogan <chasecoogan12@gmail.com>
2023-02-14 15:24:06 -0500

    comboBox setup

--- src/main/java/com/example/sorting/Controller.java
+++ src/main/java/com/example/sorting/Controller.java
@@ -1,19 +1,27 @@
 package com.example.sorting;
 
+import java.lang.reflect.Array;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 import javafx.animation.AnimationTimer;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.VBox;
 
 public class Controller implements Initializable {
 
@@ -32,17 +40,19 @@ public class Controller implements Initializable {
   @FXML
   Button generate;
 
+  @FXML
+  ComboBox<String> comboBox;
+
+
   ArrayList<Rectangle> rects = new ArrayList<Rectangle>();
 
   private int max;
   ArrayList<Integer> arr;
 
   public void onClick() {
-
     arr = CreateArray.createArray(max, 100);
     title.setText(arr.toString());
     this.displayRects();
-
   }
 
   private void displayRects() {
@@ -68,41 +78,44 @@ public class Controller implements Initializable {
   // Sorting Animation below -> may not use because have to do
   // both compare & swap and entire algorithm here.
 
-//  public void sortArray() {
-//    int currentIndex = 0;
-//    AnimationTimer animationTimer = new AnimationTimer() {
-//      @Override
-//      public void handle(long now) {
-//        if (currentIndex < arr.size() - 1) {
-//          int compare = arr.get(currentIndex).compareTo(arr.get(currentIndex + 1));
-//          if (compare > 0) {
-//            Collections.swap(arr, currentIndex, currentIndex + 1);
-//            displayRects();
-//            currentIndex = 0;
-//          } else {
-//            currentIndex++;
-//          }
-//        } else {
-//          this.stop();
-//        }
-//      }
-//    };
-//    animationTimer.start();
-//  }
-
   public void sortArray() {
-    BubbleSort.sortArray(arr);
-    title.setText(arr.toString());
-    displayRects();
+    final int[] currentIndex = {0};
+    AnimationTimer animationTimer = new AnimationTimer() {
+      @Override
+      public void handle(long now) {
+        if (currentIndex[0] < arr.size() - 1) {
+          int compare = arr.get(currentIndex[0]).compareTo(arr.get(currentIndex[0] + 1));
+          if (compare > 0) {
+            Collections.swap(arr, currentIndex[0], currentIndex[0] + 1);
+            displayRects();
+            currentIndex[0] = 0;
+          } else {
+            currentIndex[0]++;
+          }
+        } else {
+          this.stop();
+        }
+      }
+    };
+    animationTimer.start();
   }
 
-  public void inputEnter(){
+//  public void sortArray() {
+//    BubbleSort.sortArray(arr);
+//    title.setText(arr.toString());
+//    displayRects();
+//  }
+
+  public void inputEnter() {
     max = Integer.parseInt(arrSize.getText());
-      generate.setDisable(false);
+    generate.setDisable(false);
   }
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
     generate.setDisable(true);
+    comboBox.getItems().addAll("Bubble Sort", "Selection Sort",
+            "Insertion Sort", "Merge Sort", "Quick Sort");
   }
+
 }
